68fc8598ee4b0761e9bcb056a4aa87012c95a510
trevisharp <leonardotrevisansilio@gmail.com>
2026-03-10 09:25:45 -0300

    improve vector API

--- Betelgeuse/Vector.cs
+++ Betelgeuse/Vector.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Betelgeuse;
 
 /// <summary>
@@ -5,6 +7,40 @@ namespace Betelgeuse;
 /// </summary>
 public record Vector(float X, float Y)
 {
+    /// <summary>
+    /// X * X + Y * Y
+    /// </summary>
+    public float SqrMod
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]  
+        get => X * X + Y * Y;
+    }
+
+    /// <summary>
+    /// Sqrt(X * X + Y * Y)
+    /// </summary>
+    public float Mod
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]  
+        get => MathF.Sqrt(SqrMod);
+    }
+
+    /// <summary>
+    /// Sqrt(X * X + Y * Y)
+    /// </summary>
+    public Vector Unitary
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]  
+        get => this / Mod;
+    }
+
+    /// <summary>
+    /// Generate v = (cos(a), sin(a)) 
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]  
+    public static Vector FromAngle(float angle)
+        => new(MathF.Cos(angle), MathF.Sign(angle));
+
     public static Vector operator +(Vector v, Vector u)
         => new(v.X + u.X, v.Y + u.Y);
     
